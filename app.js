39aabbe542bb87d4d00dddb95215efbf893102e3
Ryan Shee <ryan.shee@gmail.com>
2023-12-31 16:34:09 -0800

    Add body parser

--- app.js
+++ app.js
@@ -3,20 +3,22 @@ const app = express();
 const port = 3001;
 const cors = require('cors');
 const yelp = require('yelp-fusion');
+const bodyParser = require('body-parser');
 const apiKey = 'oSldbNmG36meLbE645co-NNejWIAEuvVTexQKRgurlmHQNUYk1efTV9oPtqqIhvP73jYz_RZNdfyPmN5u14kXOPwoqm-ccgSCIJMDZq3aMaL807PDztU2BVCKrU0XXYx';
 const client = yelp.client(apiKey);
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
 });
 
-app.get('/api', (req, res) => {
-  client.search({
-    location: 'san francisco',
-  }).then(response => {
-    res.json(response.jsonBody.businesses);
+app.post('/api', (req, res) => {
+  client.search(req.body).then(response => {
+    const firstResult = response.jsonBody.businesses[0];
+    const prettyJson = JSON.stringify(firstResult, null, 4);
+    res.json(prettyJson);
   }).catch(e => {
     console.error(e);
   })
