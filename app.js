b0bc64e30508c4d46d099fac0642cb139f44d406
Ryan Shee <ryan.shee@gmail.com>
2023-12-31 16:55:35 -0800

    Hide API key teehee

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = process.env.port || 3001;
 const cors = require('cors');
 const yelp = require('yelp-fusion');
 const bodyParser = require('body-parser');
+import 'dotenv/config';
 const apiKey = process.env.API_KEY;
 const client = yelp.client(apiKey);
 
