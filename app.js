1da69e22000aaaeef4d3749e39861ef59699a022
Ryan Shee <ryan.shee@gmail.com>
2023-12-31 16:47:15 -0800

    Update port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = process.env.port || 3001;
 const cors = require('cors');
 const yelp = require('yelp-fusion');
 const bodyParser = require('body-parser');
