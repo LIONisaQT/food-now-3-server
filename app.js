e7fcd234a348abc28e772d8d4591a5f72518e3e9
Ryan Shee <ryan.shee@gmail.com>
2024-01-07 18:38:23 -0800

    Send random results, reroll scaffolding

--- app.js
+++ app.js
@@ -15,14 +15,40 @@ app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
 });
 
+function getRandomResult(results) {
+  const random = Math.floor(Math.random() * results.length);
+  const result = results[random];
+  return JSON.stringify(result, null, 2);
+}
+
 app.post('/api', (req, res) => {
+  console.table(req.body);
+
   client.search(req.body).then(response => {
-    const firstResult = response.jsonBody.businesses[0];
-    const prettyJson = JSON.stringify(firstResult, null, 4);
-    res.json(prettyJson);
+    let results = response.jsonBody.businesses;
+
+    let rating = req.body.rating;
+    if (rating > 4.5) rating = 4.5; // Fudge results a little
+    results = results.filter(result => result.rating >= rating);
+
+    const result = getRandomResult(results);
+    res.json(result);
   }).catch(e => {
     console.error(e);
   })
+});
+
+app.post('/reroll', (req, res) => {
+  res.json({
+    message: 'This endpoint is currently unavailable.'
+  });
+  return;
+
+  // TODO: Send results (in addition to final pick) to user. On reroll, send results array back here for processing.
+  let results = req.body.results;
+  results = results.filter(result => result.id !== req.body.id);
+  const result = getRandomResult(results);
+  res.json(result);
 });
 
 module.exports = app;
